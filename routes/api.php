5382c3977f381aac85d0bdc8b8013ccc0bb9014c
akmalranyan <akmalranyan@gmail.com>
2023-03-22 13:29:56 +0700

    added middleware for index and show

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,10 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::middleware('auth::sanctum')->group(function(){
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+});
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
 
 Route::post('/login', [AuthenticationController::class, 'login']);
