dcc696c17da687ba19b5dcd0099739e80776405d
akmalranyan <akmalranyan@gmail.com>
2023-03-24 10:11:30 +0700

    created image column to products table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,7 +16,8 @@ class Product extends Model
         'price',
         'desc',
         'owner',
-        'type'
+        'type',
+        'image'
     ];
 
     public function seller(): BelongsTo
